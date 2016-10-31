336aa7f09d62c13adbd98d623a2323da19dcee7f
ticki <ticki@users.noreply.github.com>
2016-10-31 20:19:14 +0100

    Implement LZ4 compression.
    We implement LZ4 compression and decompression.

---
+++ lz4/src/lib.rs
@@ -0,0 +1,13 @@
+//! Pure Rust implementation of LZ4 compression.
+//!
+//! A detailed explanation of the algorithm can be found [here](http://ticki.github.io/blog/how-lz4-works/).
+
+extern crate byteorder;
+
+mod decompress;
+mod compress;
+#[cfg(test)]
+mod tests;
+
+pub use decompress::decompress;
+pub use compress::compress;
---
+++ lz4/src/tests.rs
@@ -0,0 +1,28 @@
+use {decompress, compress};
+
+/// Test that the compressed string decompresses to the original string.
+fn inverse(s: &str) {
+    let compressed = compress(s.as_bytes());
+    println!("Compressed '{}' into {:?}", s, compressed);
+    assert_eq!(decompress(&compressed).unwrap(), s.as_bytes());
+}
+
+#[test]
+fn shakespear() {
+    inverse("to live or not to live");
+    inverse("Love is a wonderful terrible thing");
+}
+
+#[test]
+fn totally_not_antifa_propaganda() {
+    inverse("The only good fascist is a dead fascist.");
+    inverse("bash the fash");
+    inverse("Dead fascists can't vote.");
+    inverse("Good night, white pride.");
+    inverse("Some say fascism started with gas chambers. I say that's where it ends.");
+}
+
+#[test]
+fn not_compressible() {
+    inverse("as6yhol.;jrew5tyuikbfewedfyjltre22459ba");
+}
