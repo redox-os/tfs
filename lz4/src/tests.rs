c615d32c49423c6a8a38a8c74a9f1d6b412dc4c3
ticki <ticki@users.noreply.github.com>
2016-11-01 23:09:04 +0100

    Add tests for the compression ratio and longer texts.
    - Rename to lz4-compress.

--- lz4/src/tests.rs
+++ lz4/src/tests.rs
@@ -40,3 +40,12 @@ fn short() {
 fn empty_string() {
     inverse("");
 }
+
+#[test]
+fn compression_works() {
+    let s = "micah (Micah Cohen, politics editor): Clinton’s lead has shrunk to a hair above 4 percentage points in our polls-only model, down from about 7 points two weeks ago. So we find ourselves in an odd position where Clinton still holds a clear lead, but it’s shrinking by the day. I’ve been getting questions from Clinton supporters wondering how panicked they should be, and while we advise everyone of all political stripes to always remain calm, let’s try to answer that question today. How safe is Clinton’s lead/how panicked should Democrats be? As tacky as it is to cite your own tweet, I’m going to do it anyway — here’s a handy scale: natesilver: It’s uncertain, in part, because of the risk of a popular vote-Electoral College split. And, in part, because there are various reasons to think polling error could be high this year, such as the number of undecided voters. You can see those forces at play in the recent tightening. Clinton hasn’t really declined very much in these latest polls. But she was at only 46 percent in national polls, and that left a little bit of wiggle room for Trump.";
+
+    inverse(s);
+
+    assert!(compress(s.as_bytes()).len() < s.len());
+}
