71b337e6cfbb21dce376c2033a50c63601627b6e
ticki <ticki@users.noreply.github.com>
2016-11-24 23:22:45 +0100

    Handle unaligned hashes more efficiently (group bytes together).
    This commit makes SeaHash more general purpose.

--- seahash/src/lib.rs
+++ seahash/src/lib.rs
@@ -78,34 +78,31 @@ use core::num::Wrapping as W;
 fn diffuse(mut x: W<u64>) -> W<u64> {
     x = x ^ (x >> 32);
     x = x * W(0x7ed0e9fa0d94a33);
-    x = x ^ (x >> 32);
-    x = x * W(0x7ed0e9fa0d94a33);
-    x = x ^ (x >> 32);
 
     x
 }
 
 /// Hash some buffer.
 pub fn hash(buf: &[u8]) -> u64 {
-    // We use 4 different registers to store seperate hash states, because this allows us to update
-    // them seperately, and consequently exploiting ILP to update the states in parallel.
-    let mut a = W(0x16f11fe89b0d677c);
-    let mut b = W(0xb480a793d8e6c86c);
-    let mut c = W(0x6fe2e5aaf078ebc9);
-    let mut d = W(0x14f994a4c5259381);
+    unsafe {
+        // We use 4 different registers to store seperate hash states, because this allows us to update
+        // them seperately, and consequently exploiting ILP to update the states in parallel.
+        let mut a = W(0x16f11fe89b0d677c);
+        let mut b = W(0xb480a793d8e6c86c);
+        let mut c = W(0x6fe2e5aaf078ebc9);
+        let mut d = W(0x14f994a4c5259381);
 
-    // We fetch this in order to avoid aliasing things later on and thus breaking certain
-    // optimizations.
-    let len = buf.len() as isize;
-    // We round down to a multiple of 32.
-    let mut written_len = len & !0x1F;
-    // We pre-fetch the pointer to the buffer to avoid too many cache misses.
-    let buf_ptr = buf.as_ptr();
-    // The pointer to the current bytes.
-    let mut ptr = unsafe { buf_ptr.offset(written_len) } as *const u64;
+        // We fetch this in order to avoid aliasing things later on and thus breaking certain
+        // optimizations.
+        let len = buf.len() as isize;
+        // We round down to a multiple of 32.
+        let mut written_len = len & !0x1F;
+        // We pre-fetch the pointer to the buffer to avoid too many cache misses.
+        let buf_ptr = buf.as_ptr();
+        // The pointer to the current bytes.
+        let mut ptr = buf_ptr.offset(written_len) as *const u64;
 
-    while (buf_ptr as usize) < ptr as usize {
-        unsafe {
+        while (buf_ptr as usize) < ptr as usize {
             // Read and diffuse the next 4 64-bit little-endian integers from their bytes. Note
             // that we on purpose not use `+=` and co., because it aliases the lvalue, making it
             // harder for LLVM to register allocate (it will have to inline the value behind the
@@ -127,34 +124,154 @@ pub fn hash(buf: &[u8]) -> u64 {
 
             ptr = ptr.offset(-1);
             d = d + W((*ptr).to_le());
+
+            // Diffuse the updated registers. We hope that each of these are executed in parallel.
+            a = diffuse(a);
+            b = diffuse(b);
+            c = diffuse(c);
+            d = diffuse(d);
         }
 
-        // Diffuse the updated registers. We hope that each of these are executed in parallel.
-        a = diffuse(a);
-        b = diffuse(b);
-        c = diffuse(c);
-        d = diffuse(d);
-    }
+        // Handle the excessive bytes.
+        let mut excessive = len - written_len;
+        if excessive != 0 {
+            if excessive >= 24 {
+                // 24 bytes or more excessive.
 
-    // Diffuse `b` and `d` into `a` and `c`.
-    a = a + diffuse(b);
-    c = c + diffuse(d);
+                // Update `a`.
+                ptr = ptr.offset(-1);
+                a = a + W((*ptr).to_le());
+                // Update `b`.
+                ptr = ptr.offset(-1);
+                b = b + W((*ptr).to_le());
+                // Update `c`.
+                ptr = ptr.offset(-1);
+                c = c + W((*ptr).to_le());
 
-    // Diffuse `a` and `c`.
-    let mut ret = a + diffuse(c);
+                // Diffuse `a`, `b`, and `c`.
+                a = diffuse(a);
+                b = diffuse(b);
+                c = diffuse(c);
 
-    // Handle the excessive bytes.
-    while written_len < len {
-        // Increment the written bytes counter.
-        written_len = written_len + 1;
+                // We just wrote 8 bytes.
+                written_len = written_len + 24;
+            } else if excessive >= 16 {
+                // 16 bytes or more excessive.
 
-        unsafe {
-            // Update the return value by adding the excessive byte and then diffusing.
-            ret = diffuse(ret + W(*buf_ptr.offset(written_len) as u64));
+                // Update `a`.
+                ptr = ptr.offset(-1);
+                a = a + W((*ptr).to_le());
+                // Update `b`.
+                ptr = ptr.offset(-1);
+                b = b + W((*ptr).to_le());
+
+                // Diffuse `a` and `b`.
+                a = diffuse(a);
+                b = diffuse(b);
+
+                // We just wrote 8 bytes.
+                written_len = written_len + 16;
+            } else if excessive >= 8 {
+                // 8 bytes or more excessive.
+
+                // Update `a`.
+                ptr = ptr.offset(-1);
+                a = a + W((*ptr).to_le());
+                // Diffuse `a`.
+                a = diffuse(a);
+
+                // We just wrote 8 bytes.
+                written_len = written_len + 8;
+            }
+
+            // Update the excessive byte count.
+            excessive = len - written_len;
+            // We now have < 8 bytes excessive.
+            match excessive {
+                1 => {
+                    // Exactly one byte is excessive. Read this byte and diffuse it into `a`.
+                    a = diffuse(a + W(*buf_ptr.offset(written_len) as u64));
+                },
+                2 => {
+                    // Two bytes (an u16) is excessive. We diffuse this into `a`.
+                    a = diffuse(a + W((*(buf_ptr.offset(written_len) as *const u16)).to_le() as u64));
+                },
+                3 => {
+                    // An u16 and an u8 are excessive. We diffuse this into `a` and `b` respectively.
+
+                    // Mix the two first bytes into `a`.
+                    a = a + W((*(buf_ptr.offset(written_len) as *const u16)).to_le() as u64);
+                    // Update the written bytes counter.
+                    written_len = written_len + 2;
+                    // Mix the last byte into `b`.
+                    b = b + W(*buf_ptr.offset(written_len) as u64);
+
+                    // Diffuse the states.
+                    a = diffuse(a);
+                    b = diffuse(b);
+                },
+                4 => {
+                    // An u32 is excessive. We diffuse this into `a`.
+                    a = diffuse(a + W((*(buf_ptr.offset(written_len) as *const u32)).to_le() as u64));
+                },
+                5 => {
+                    // An u32 and an u8 are excessive. We diffuse this into `a` and `b`, respectively.
+
+                    // Mix the first 4 bytes into `a`.
+                    a = a + W((*(buf_ptr.offset(written_len) as *const u32)).to_le() as u64);
+                    // Update the written bytes counter.
+                    written_len = written_len + 4;
+                    // Mix the last byte into `b`.
+                    b = b + W(*buf_ptr.offset(written_len) as u64);
+
+                    // Diffuse the states.
+                    a = diffuse(a);
+                    b = diffuse(b);
+                },
+                6 => {
+                    // An u32 and an u16 are excessive. We diffuse this into `a` and `b`, respectively.
+
+                    // Mix the first 4 bytes into `a`.
+                    a = a + W((*(buf_ptr.offset(written_len) as *const u32)).to_le() as u64);
+                    // Update the written bytes counter.
+                    written_len = written_len + 4;
+                    // Mix the last byte into `b`.
+                    b = b + W((*(buf_ptr.offset(written_len) as *const u16)).to_le() as u64);
+
+                    // Diffuse the states.
+                    a = diffuse(a);
+                    b = diffuse(b);
+                },
+                7 => {
+                    // An u32, an u16, and an u8 are excessive. We diffuse this into `a`, `b`, and `c`, respectively.
+
+                    // Mix the first 4 bytes into `a`.
+                    a = a + W((*(buf_ptr.offset(written_len) as *const u32)).to_le() as u64);
+                    // Update the written bytes counter.
+                    written_len = written_len + 4;
+                    // Mix the next 2 bytes into `b`.
+                    b = b + W((*(buf_ptr.offset(written_len) as *const u16)).to_le() as u64);
+                    // Update the written bytes counter.
+                    written_len = written_len + 2;
+                    // Mix the last byte into `c`.
+                    c = c + W(*buf_ptr.offset(written_len) as u64);
+
+                    // Diffuse the states.
+                    a = diffuse(a);
+                    b = diffuse(b);
+                    c = diffuse(c);
+                },
+                _ => {},
+            }
         }
-    }
 
-    ret.0
+        // Diffuse `b` and `d` into `a` and `c`.
+        a = a + diffuse(b);
+        c = c + diffuse(d);
+
+        // Diffuse `a` and `c`.
+        (a + diffuse(c) + W(written_len as u64)).0
+    }
 }
 
 #[cfg(test)]
@@ -163,7 +280,7 @@ mod tests {
 
     #[test]
     fn zero() {
-        assert_eq!(hash(&[0; 4096]), 6601654217258774873);
+        assert_eq!(hash(&[0; 4096]), 16549744396473422879);
     }
 
     #[test]
@@ -172,7 +289,7 @@ mod tests {
         for i in 0..4096 {
             buf[i] = i as u8;
         }
-        assert_eq!(hash(&buf), 10263305321322108915);
+        assert_eq!(hash(&buf), 11224124121938825130);
     }
 
     #[test]
@@ -191,12 +308,29 @@ mod tests {
 
     #[test]
     fn shakespear() {
-        assert_eq!(hash(b"to be or not to be"), 11897643864797330044);
+        assert_eq!(hash(b"to be or not to be"), 9129472795863565305);
+    }
+
+    #[test]
+    fn zero_senitive() {
+        assert_ne!(hash(&[1, 2, 3, 4]), hash(&[1, 0, 2, 3, 4]));
+        assert_ne!(hash(&[1, 2, 3, 4]), hash(&[1, 0, 0, 2, 3, 4]));
+        assert_ne!(hash(&[1, 2, 3, 4]), hash(&[1, 2, 3, 4, 0]));
+        assert_ne!(hash(&[1, 2, 3, 4]), hash(&[0, 1, 2, 3, 4]));
+        assert_ne!(hash(&[0, 0, 0]), hash(&[0, 0, 0, 0, 0]));
     }
 
     #[test]
     fn not_equal() {
         assert_ne!(hash(b"to be or not to be "), hash(b"to be or not to be"));
         assert_ne!(hash(b"jkjke"), hash(b"jkjk"));
+        assert_ne!(hash(b"ijkjke"), hash(b"ijkjk"));
+        assert_ne!(hash(b"iijkjke"), hash(b"iijkjk"));
+        assert_ne!(hash(b"iiijkjke"), hash(b"iiijkjk"));
+        assert_ne!(hash(b"iiiijkjke"), hash(b"iiiijkjk"));
+        assert_ne!(hash(b"iiiiijkjke"), hash(b"iiiiijkjk"));
+        assert_ne!(hash(b"iiiiiijkjke"), hash(b"iiiiiijkjk"));
+        assert_ne!(hash(b"iiiiiiijkjke"), hash(b"iiiiiiijkjk"));
+        assert_ne!(hash(b"iiiiiiiijkjke"), hash(b"iiiiiiiijkjk"));
     }
 }
