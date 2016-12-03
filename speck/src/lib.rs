8ee5e78a9bb54373c9900641368cd05be93cfd1c
ticki <ticki@users.noreply.github.com>
2016-12-03 16:42:10 +0100

    Precompute the key schedule in Speck.
    This should make our implementation slightly faster.

--- speck/src/lib.rs
+++ speck/src/lib.rs
@@ -31,56 +31,65 @@ macro_rules! inv_round {
     }
 }
 
-/// A key, which can be used to encrypt and decrypt messages.
+/// Encrypt a block with key schedule generated on-the-go.
+///
+/// This works great for one-time use of a key (such as usages other than encryption), because it
+/// should never read from memory (both the message and the keys are stored in the registers). As
+/// such, this should be really fast for such usage.
+///
+/// If you want to reuse the key, however, it is recommended that you use the precomputed schedule
+/// provided by the `Key` struct.
+pub fn encrypt_block((mut m1, mut m2): (u64, u64), (mut k1, mut k2): (u64, u64)) -> (u64, u64) {
+    // Run the initial round (similar to the loop below, but doesn't update the key schedule).
+    round!(m1, m2, k2);
+
+    for i in 0..ROUNDS - 1 {
+        // Progress the key schedule.
+        round!(k1, k2, i);
+        // Run a round over the message.
+        round!(m1, m2, k2);
+    }
+
+    (m1, m2)
+}
+
+/// A precomputed key.
+///
+/// This precomputes a key schedule, which can then be used for both encrypting and decrypting messages.
 pub struct Key {
-    /// The left ending subkey of the schedule.
-    end_k1: u64,
-    /// The right ending subkey of the schedule.
-    end_k2: u64,
-    /// The left starting subkey of the schedule.
-    start_k1: u64,
-    /// The right starting subkey of the schedule.
-    start_k2: u64,
+    /// The computed schedule.
+    ///
+    /// Each of these subkeys are used in a round of the cipher. The first subkey is used in the
+    /// first round of the cipher and so on.
+    schedule: [u64; ROUNDS as usize],
 }
 
 impl Key {
     /// Generate a new key from some seed.
     pub fn new(mut k1: u64, mut k2: u64) -> Key {
         let mut ret = Key {
-            start_k1: k1,
-            start_k2: k2,
-            end_k1: 0,
-            end_k2: 0,
+            schedule: [0; ROUNDS as usize],
         };
 
         // Run `ROUNDS - 1` rounds to generate the key's endpoint (the last key in the schedule).
-        for i in 0..ROUNDS - 1 {
+        for i in 0..ROUNDS {
+            // Insert the key into the schedule.
+            ret.schedule[i as usize] = k2;
+
             // The beautiful thing about Speck is that it reuses its round function to generate the
             // key schedule.
             round!(k1, k2, i);
         }
-
-        // Set the generated keys as the endpoints.
-        ret.end_k1 = k1;
-        ret.end_k2 = k2;
 
         ret
     }
 
     /// Encrypt a 128-bit block with this key.
     pub fn encrypt_block(&self, (mut m1, mut m2): (u64, u64)) -> (u64, u64) {
-        // Fetch the keys.
-        let mut k1 = self.start_k1;
-        let mut k2 = self.start_k2;
-
-        // Run the initial round (similar to the loop below, but doesn't update the key schedule).
-        round!(m1, m2, k2);
-
-        for i in 0..ROUNDS - 1 {
-            // Progress the key schedule.
-            round!(k1, k2, i);
-            // Run a round over the message.
-            round!(m1, m2, k2);
+        // We run a round for every subkey in the generated key schedule.
+        for &k in &self.schedule {
+            // Run a round on the message.
+            round!(m1, m2, k);
         }
 
         (m1, m2)
@@ -88,22 +97,11 @@ impl Key {
 
     /// Decrypt a 128-bit block with this key.
     pub fn decrypt_block(&self, (mut c1, mut c2): (u64, u64)) -> (u64, u64) {
-        // Fetch the endpoint keys.
-        let mut k1 = self.end_k1;
-        let mut k2 = self.end_k2;
-
-        for i in (0..ROUNDS - 1).rev() {
-            // Run a round over the message.
-            inv_round!(c1, c2, k2);
-            // Revert the key update round.
-            inv_round!(k1, k2, i);
+        // We run a round for every subkey in the generated key schedule.
+        for &k in self.schedule.iter().rev() {
+            // Run a round on the message.
+            inv_round!(c1, c2, k);
         }
-
-        debug_assert!(k1 == self.start_k1 && k2 == self.start_k2, "The start keys does not match \
-                      the key derived from the end key.");
-
-        // Revert the initial round.
-        inv_round!(c1, c2, k2);
 
         (c1, c2)
     }
@@ -129,14 +127,13 @@ mod tests {
             let key = Key::new(x, y);
 
             assert_eq!(key.decrypt_block(key.encrypt_block((a, b))), (a, b));
+            assert_eq!(key.encrypt_block((a, b)), encrypt_block((a, b), (x, y)));
         }
-
     }
 
     #[test]
     fn test_vectors() {
         // These test vectors are taken from the Speck paper.
-        let k = Key::new(0x0f0e0d0c0b0a0908, 0x0706050403020100);
-        assert_eq!(k.encrypt_block((0x6c61766975716520, 0x7469206564616d20)), (0xa65d985179783265, 0x7860fedf5c570d18));
+        assert_eq!(encrypt_block((0x6c61766975716520, 0x7469206564616d20), (0x0f0e0d0c0b0a0908, 0x0706050403020100)), (0xa65d985179783265, 0x7860fedf5c570d18));
     }
 }
