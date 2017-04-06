e7634c71ab8f5457ad3b53f14ff1948c4128894b
ticki <ticki@users.noreply.github.com>
2017-04-06 23:26:09 +0200

    Remove a potential use-after-free in the reclamation system.

--- reclaim/src/lib.rs
+++ reclaim/src/lib.rs
@@ -47,6 +47,13 @@ struct RawReader {
     ptr: *const T,
 }
 
+impl RawReader {
+    unsafe fn destroy(self) {
+        // Drop the atomic boolean stored on the heap.
+        mem::drop_in_place(self.active);
+    }
+}
+
 struct Reader<'a, T> {
     raw: RawReader,
     _marker: PhantomData<'a>,
@@ -59,32 +66,90 @@ impl<'a, T> Reader<'a, T> {
 }
 
 struct Atomic<T> {
-    collecting: AtomicBool,
     inner: AtomicPtr<T>,
-    readers: Stack<RawReader>,
     snapshots: Stack<Box<T>>,
+    readers: Stack<RawReader>,
+    flags: AtomicUsize,
 }
 
 impl<T> Atomic<T> {
     fn gc(&self) {
-        if !self.collecting.swap(true, Ordering::Relaxed) {
-            // Initially, every snapshot is marked unused.
-            let mut unused = self.snapshots.collect();
-
-            // Traverse the readers and update the reference counts.
-            self.readers.take_each(|reader| {
-                if reader.active.load() {
-                    // The reader is not released yet, and is thus considered active.
-
-                    // Remove the reader from the unused set and insert it back into the log, as
-                    // the snapshot is active.
-                    self.snapshots.insert(unused.remove(reader.ptr).unwrap());
-                    // Put the reader back in the structure.
-                    self.readers.insert(reader);
-                }
-            });
-
-            self.collecting.store(true);
+        // Spin until no thread is currently modifying the stacks. This prevents premature frees in
+        // the thread which is currently pushing to `self.readers`.
+        loop {
+            // Read the flags, and if no readers or garbage collectors, activate garbage
+            // collection.
+            let flags = self.flags.compare_and_swap(0, 1, atomic::Ordering::Relaxed);
+            if flags == 0 {
+                // Currently, noone accesses the readers stack and the CAS above means that the
+                // lowest bitflag have been set, indicating that a garbage collection is now
+                // active.
+                break;
+            } else if flags & 1 == 1 {
+                // Another thread is currently garbage collection. No need for this thread doing
+                // the same.
+                return;
+            }
         }
+
+        // We've now set the lowest bit in `self.active` namely the flag defining if we are
+        // collecting.
+
+        // Initially, every snapshot is marked unused.
+        let mut unused = self.snapshots.collect();
+
+        // Traverse the readers and update the reference counts.
+        self.readers.take_each(|reader| {
+            if reader.active.load() {
+                // The reader is not released yet, and is thus considered active.
+
+                // Remove the reader from the unused set and insert it back into the log, as the
+                // snapshot is active.
+                self.snapshots.insert(unused.remove(reader.ptr).unwrap());
+                // Put the reader back in the structure.
+                self.readers.insert(reader);
+            } else {
+                // The reader was released. Destroy it.
+                reader.destroy();
+            }
+        });
+
+        // Unset the bitflag defining if currently garbage collecting.
+        self.flags.fetch_and(!1, atomic::Ordering::Relaxed);
+    }
+
+    fn get(&self) -> Reader {
+        // Increment the number of threads currently pushing to the readers stack. We add two to
+        // account for the LSB being a separate bitflag. This ensures that the read snapshot isn't
+        // freed while registering it in the reader stack.
+        self.flags.fetch_add(2, atomic::Ordering::Relaxed);
+
+        // Construct the raw reader.
+        let reader = RawReader {
+            // Load a snapshot of the pointer.
+            ptr: self.inner.load(atomic::Ordering::Relaxed),
+            // We allocate the atomic boolean on the heap as it is shared between the returned RAII
+            // guard and the reader stack.
+            released: Box::from_raw(Box::new(AtomicBool::new(false))),
+        };
+
+        // Register the reader through the reader stack, ensuring that it is not freed before the
+        // RAII guard drops (`reader.release` is set to `true`).
+        self.readers.push(reader);
+
+        // Revert the original increment.
+        self.flags.fetch_sub(2, atomic::Ordering::Relaxed);
+
+        Reader {
+            raw: reader,
+            _marker: PhantomData,
+        }
+    }
+
+    fn set(&self, new: Box<T>) {
+        // Replace the inner by the new value.
+        let old = self.inner.swap(Box::into_raw(new), atomic::Ordering::Relaxed);
+        // Push the old pointer to the snapshot stack.
+        self.snapshots.push(Box::from_raw(old));
     }
 }
