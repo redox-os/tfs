5895fefdcc5ebe1dcf190a76bd4dada3f93f6b66
ticki <ticki@users.noreply.github.com>
2017-04-15 14:38:14 +0200

    Reorganize the runtime of the GC.
    - Rename `Reader` to `Linking`, snapshot to link, garbage to
      unreachable, etc.
    - Extend the rt API.

--- reclaim/src/lib.rs
+++ reclaim/src/lib.rs
@@ -2,15 +2,37 @@ mod treiber;
 mod rt;
 
 use std::sync::atomic::{self, AtomicPtr};
+use std::ops;
 
 struct Snapshot<'a, T> {
-    raw: RawSnapshot,
+    link: rt::Link,
     _marker: PhantomData<'a>,
 }
 
-impl<'a, T> Snapshot<'a, T> {
+impl<'a, T> ops::Deref for Snapshot<'a, T> {
+    fn deref(&self) -> &T {
+        &*self.ptr
+    }
+}
+
+impl<'a, T> Drop for Reader<'a, T> {
     fn drop(&mut self) {
-        self.raw.active.store(true);
+        self.link.set_inactive();
+    }
+}
+
+impl rt::Linking {
+    pub fn load<T>(&self, a: &Atomic<T>) -> Snapshot<T> {
+        // Construct a link to the inner pointer.
+        let link = rt::Link::new(a.load(atomic::Ordering::Relaxed));
+
+        // Ensure that the object isn't destroyed while a RAII reader exists.
+        self.link(link);
+
+        Snapshot {
+            link: link,
+            _marker: PhantomData,
+        }
     }
 }
 
@@ -26,15 +48,13 @@ impl<T> Atomic<T> {
     }
 
     pub fn load(&self) -> Snapshot<T> {
-        read(|r| r.load(self))
+        rt::linking(|r| r.load(self))
     }
 
     pub fn store(&self, new: Box<T>) {
         // Replace the inner by the new value.
         let old = self.inner.swap(Box::into_raw(new), atomic::Ordering::Relaxed);
-        // Push the old pointer to the garbage stack.
-        GARBAGE.push(Box::from_raw(old));
-
-        tick();
+        // The old value is now unreachable, so we must mark it so.
+        rt::set_unreachable(old);
     }
 }
--- reclaim/src/rt.rs
+++ reclaim/src/rt.rs
@@ -1,13 +1,46 @@
 use std::cell::Cell;
 
-static GARBAGE: Stack<Box<Drop>> = Stack::new();
-static ACTIVE_SNAPSHOTS: Stack<RawSnapshot> = Stack::new();
-// TODO: rename, name's shite
-static PUSHING_TO_ACTIVE_SNAPSHOTS: AtomicUsize = AtomicUsize::new(0);
+static UNREACHABLE: Stack<Box<Drop>> = Stack::new();
+static LINKS: Stack<Link> = Stack::new();
+static ADDING_LINKS: AtomicUsize = AtomicUsize::new(0);
 static TICKS_BEFORE_GC: u16 = 2000;
 
 thread_local!(static CLOCK: Cell<u16> = Cell::new(0));
 thread_local!(static ACTIVE_READERS: Cell<usize> = Cell::new(0));
+
+struct Link<T> {
+    active: *const AtomicBool,
+    pub ptr: *const T,
+}
+
+impl Link {
+    fn new(ptr: *const T) {
+        Link {
+            ptr: ptr,
+            // We allocate the atomic boolean on the heap as it is shared between the returned RAII
+            // guard and the reader stack.
+            released: Box::into_raw(Box::new(AtomicBool::new(false))),
+        }
+    }
+
+    fn set_inactive(&self) {
+        self.active.store(true);
+    }
+
+    unsafe fn destroy(self) {
+        // Drop the atomic boolean stored on the heap.
+        mem::drop_in_place(self.active);
+    }
+}
+
+/// Set an item as unreachable.
+///
+/// "Unreachable" means that no new links to the item can be added. In other words, the only active
+/// links are the one that were obtained prior to the item being marked unreachable.
+pub fn set_unreachable<T>(item: Box<T>) {
+    UNREACHABLE.push(item);
+    tick();
+}
 
 pub fn gc() {
     // Check if there are any active readers.
@@ -19,27 +52,27 @@ pub fn gc() {
     // Set the clock back to zero to delay the next garbage collection.
     CLOCK.set(0);
 
-    // Spin until no other thread is pushing snapshots, ensuring the stack of active snapshot is
-    // not incomplete, and therefore collectable.
-    while PUSHING_TO_ACTIVE_SNAPSHOTS.compare_and_swap(0, 1) != 0 {}
+    // Spin until no other thread is adding links, ensuring the stack of active links is not
+    // incomplete, and therefore collectable.
+    while ADDING_LINKS.compare_and_swap(0, 1) != 0 {}
 
-    // Initially, every garbage is marked unused.
-    let mut unused = GARBAGE.collect();
-    let snapshots = ACTIVE_SNAPSHOTS.take();
+    // Initially, all the unreachable items are marked unused.
+    let mut unused = UNREACHABLE.collect();
+    let links = LINKS.take();
 
     // Set back the counter as our collection of the stacks is over.
-    PUSHING_TO_ACTIVE_SNAPSHOTS.store(0);
+    ADDING_LINKS.store(0);
 
-    // Traverse the active snapshots and update the reference counts.
-    snapshots.for_each(|reader| {
+    // Traverse the active links and update the set of unused items.
+    links.for_each(|reader| {
         if reader.active.load() {
             // The reader is not released yet, and is thus considered active.
 
-            // Remove the reader from the unused set and insert it back into the log (if it
-            // exists in the unused set), as the garbage is active.
-            unused.remove(reader.ptr).map(|x| GARBAGE.insert(x));
+            // Remove the reader from the unused set and insert it back into the log (if it exists
+            // in the unused set), as the link is active.
+            unused.remove(reader.ptr).map(|x| UNREACHABLE.insert(x));
             // Put the reader back in the structure.
-            ACTIVE_SNAPSHOTS.insert(reader);
+            LINKS.insert(reader);
         } else {
             // The reader was released. Destroy it.
             reader.destroy();
@@ -56,58 +89,30 @@ pub fn tick() {
     }
 }
 
-pub fn read<T, F>(f: F) -> T
-where F: Fn(Reader) -> T {
+pub fn linking<T, F>(f: F) -> T
+where F: Fn(Linking) -> T {
     let mut active = ACTIVE_READERS.get();
     ACTIVE_READERS.set(active + 1);
     if active == 0 {
-        PUSHING_TO_ACTIVE_SNAPSHOTS.fetch_add(1);
+        ADDING_LINKS.fetch_add(1);
     }
 
-    let reader = Reader;
-    let ret = f(&reader);
+    let linking = Linking;
+    let ret = f(linking);
 
     active = ACTIVE_READERS.get();
     if active == 1 {
-        PUSHING_TO_ACTIVE_SNAPSHOTS.fetch_sub(1);
+        ADDING_LINKS.fetch_sub(1);
     }
     ACTIVE_READERS.set(active - 1);
 
     ret
 }
 
-pub struct Reader;
+struct Linking;
 
-impl Reader {
-    pub fn load<T>(&self, a: &Atomic<T>) -> Snapshot<T> {
-        // Construct the raw reader.
-        let reader = RawSnapshot {
-            // Load a snapshot of the pointer.
-            ptr: self.inner.load(atomic::Ordering::Relaxed),
-            // We allocate the atomic boolean on the heap as it is shared between the returned RAII
-            // guard and the reader stack.
-            released: Box::into_raw(Box::new(AtomicBool::new(false))),
-        };
-
-        // Register the reader through the reader stack, ensuring that it is not freed before the
-        // RAII guard drops (`reader.release` is set to `true`).
-        ACTIVE_SNAPSHOTS.push(reader);
-
-        Snapshot {
-            raw: reader,
-            _marker: PhantomData,
-        }
-    }
-}
-
-struct RawSnapshot<T> {
-    active: *const AtomicBool,
-    ptr: *const T,
-}
-
-impl RawSnapshot {
-    unsafe fn destroy(self) {
-        // Drop the atomic boolean stored on the heap.
-        mem::drop_in_place(self.active);
+impl Linking {
+    pub fn link(&self, item: Link) {
+        LINKS.push(item);
     }
 }
