a54396e7f517ff366399235873c18e23e4b3c2c4
ticki <ticki@users.noreply.github.com>
2017-05-26 18:34:40 +0200

    Get rid of warnings in `tm`.

--- tm/src/lib.rs
+++ tm/src/lib.rs
@@ -1,32 +1,29 @@
 extern crate concurrent;
 
-use std::sync::atomic::{self, AtomicPtr};
-use std::mem;
+use std::sync::atomic;
 
-const ORDERING: atomic::Ordering = atomic::Ordering::Relaxed;
-
-struct Memory<T> {
+pub struct Memory<T> {
     inner: concurrent::Option<T>,
 }
 
 impl<T> Memory<T> {
-    fn new(data: Option<Box<T>>) -> Memory<T> {
+    pub fn new(data: Option<Box<T>>) -> Memory<T> {
         Memory {
             inner: concurrent::Option::new(data),
         }
     }
 
-    fn with<F>(&self, f: F)
+    pub fn with<F>(&self, f: F)
     where
         F: Fn(Option<concurrent::Guard<T>>) -> Option<Box<T>>,
         T: 'static,
     {
         loop {
-            let snapshot = self.inner.load(ORDERING);
+            let snapshot = self.inner.load(atomic::Ordering::Relaxed);
             let snapshot_ptr = snapshot.as_ref().map(concurrent::Guard::as_raw);
             let ret = f(snapshot);
 
-            if self.inner.compare_and_store(snapshot_ptr, ret, ORDERING).is_ok() {
+            if self.inner.compare_and_store(snapshot_ptr, ret, atomic::Ordering::Relaxed).is_ok() {
                 break;
             }
         }
